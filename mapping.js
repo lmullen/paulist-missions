a90f7e40374536d74aa6735a50551e699c2bdce5
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-10-29 10:39:01 -0400

    Add classes to state outlines

--- mapping.js
+++ mapping.js
@@ -1,6 +1,10 @@
 var width = window.innerWidth,
     height = 560;
 
+var projection = d3.geo.albersUsa();
+
+var path = d3.geo.path().projection(projection);
+
 var svg = d3.select("body").append("svg")
   .attr("width", width)
   .attr("height", height);
@@ -8,13 +12,14 @@ var svg = d3.select("body").append("svg")
 
 d3.json("state_1870.json", function(error, state_1870) {
 
-  var subunits = topojson.feature(state_1870, state_1870.objects.states);
+  // var subunits = topojson.feature(state_1870, state_1870.objects.states);
+  // svg.append("path").datum(subunits).attr("d", path);
 
-  var projection = d3.geo.albersUsa();
-
-  var path = d3.geo.path().projection(projection);
-
-  svg.append("path").datum(subunits).attr("d", path);
+  svg.selectAll(".states")
+    .data(topojson.feature(state_1870, state_1870.objects.states).features)
+    .enter().append("path")
+    .attr("class", function(d) { return "subunit " + d.id; })
+    .attr("d", path);
 
 });
 
