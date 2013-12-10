8305607d995bdfa3c7245453d2e28f0834750008
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-12-09 19:42:54 -0500

    Divided missions into 3 maps; created base map variable

--- map-missions.r
+++ map-missions.r
@@ -11,22 +11,18 @@ library(maps)
 missions <- read.csv("demographics-religion/data/paulist-chronicles/paulist-missions.geocoded.csv", 
                      stringsAsFactors = FALSE)
 
-# Defaults for maps 
-my_theme <- theme_tufte() +
-  theme(panel.border = element_blank(),
-        axis.ticks   = element_blank(),
-        axis.text    = element_blank(),
-        axis.title   = element_blank())
-
 # Maps 
 canada <- map_data("world", "Canada")
 mexico <- map_data("world", "Mexico")
 us     <- map_data("state")
 
-# Map of missions before Civil War
-missions_precw <- subset(missions, year < 1866)
+# Three datasets
+missions_1 <- subset(missions, year < 1866)
+missions_2 <- subset(missions, year >= 1866 & year <= 1882 )
+missions_3 <- subset(missions, year >= 1883)
 
-map_precw <- ggplot() +
+# Base map
+base_map <- ggplot() +
   coord_map() +
   geom_path(data = us, 
             aes(x = long, y = lat, group = group),
@@ -35,47 +31,57 @@ map_precw <- ggplot() +
             color = 'gray', fill = 'white', size = .3) +
   geom_path(data = mexico, aes(x = long, y = lat, group = group),
             color = 'gray', fill = 'white', size = .3) +
-  xlim(-93,-65) +
-  ylim(23, 49) +
-  geom_point(data = missions_precw,
-             aes(x = geo.lon, y=geo.lat, size = converts),
-             alpha = 0.5) +
-  ggtitle("Redemptorist and Paulist Missions, 1852-1865 ") +
-  my_theme +
+  theme_tufte() +
+  theme(panel.border = element_blank(),
+        axis.ticks   = element_blank(),
+        axis.text    = element_blank(),
+        axis.title   = element_blank()) +
   guides(size=guide_legend(title="Converts\nper mission")) +
   scale_size(range = c(3, 8))
 
-pdf(file = "paulists-map-pre-civil-war.pdf",
-    height = 8.5, width = 11)
-print(map_precw)
-dev.off()
 
-# Map after the Civil War
-missions_postcw <- subset(missions, year >= 1866 )
+# Map 1
+map_1 <- base_map +
+  xlim(-93,-65) +
+  ylim(23, 49) +
+  geom_point(data = missions_1,
+             aes(x = geo.lon, y=geo.lat, size = converts_total),
+             alpha = 0.5) +
+  ggtitle("Redemptorist and Paulist Missions, 1852-1865") +
 
-map_postcw <- ggplot() +
-  coord_map() +
-  geom_path(data = us, 
-            aes(x = long, y = lat, group = group),
-            color = 'gray', fill = 'white', size = .2) +
-  geom_path(data = canada, aes(x = long, y = lat, group = group),
-            color = 'gray', fill = 'white', size = .2) +
-  geom_path(data = mexico, aes(x = long, y = lat, group = group),
-            color = 'gray', fill = 'white', size = .2) +
+# pdf(file = "paulists-map-pre-civil-war.pdf",
+#     height = 8.5, width = 11)
+print(map_1)
+# dev.off()
+
+# Map 2
+map_2 <- base_map +
 #   geom_path(data = rail, aes(x = long, y= lat, group = group),
 #             color = 'black', alpha = .5, size = .3) +
   xlim(-126,-65) +
   ylim(23, 51) +
-  geom_point(data = missions_postcw,
-             aes(x = geo.lon, y=geo.lat, size = converts),
+  geom_point(data = missions_2,
+             aes(x = geo.lon, y=geo.lat, size = converts_total),
              alpha = 0.5) +
-  my_theme +
-  theme(legend.position="bottom") +
-  scale_size(range = c(3, 8)) +
-  guides(size=guide_legend(title="Converts\nper mission")) +
-  ggtitle("Paulist Missions 1871-1886, with Railroads in 1870")
+  ggtitle("Paulist Missions 1871-1882, with Railroads in 1870")
 
-pdf(file = "outputs/paulists/paulists-map-post-civil-war.pdf",
-    height = 8.5, width= 11)
-print(map_postcw)
-dev.off()
+# pdf(file = "outputs/paulists/paulists-map-post-civil-war.pdf",
+#     height = 8.5, width= 11)
+print(map_2)
+# dev.off()
+
+# Map 3
+map_3 <- base_map +
+#   geom_path(data = rail, aes(x = long, y= lat, group = group),
+#             color = 'black', alpha = .5, size = .3) +
+  xlim(-126,-65) +
+  ylim(23, 51) +
+  geom_point(data = missions_3,
+             aes(x = geo.lon, y=geo.lat, size = converts_total),
+             alpha = 0.5) +
+  ggtitle("Paulist Missions 1883-1893, with Railroads in 1870")
+
+# pdf(file = "outputs/paulists/paulists-map-post-civil-war.pdf",
+#     height = 8.5, width= 11)
+print(map_3)
+# dev.off()
