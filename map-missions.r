1de80e2473a86299a0c8f79a23f586298dd57fe2
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-12-09 20:41:44 -0500

    Added railroads and PDF output

--- map-missions.r
+++ map-missions.r
@@ -7,6 +7,7 @@
 library(ggplot2)
 library(ggthemes)
 library(maps)
+library(rgdal)
 
 missions <- read.csv("demographics-religion/data/paulist-chronicles/paulist-missions.geocoded.csv", 
                      stringsAsFactors = FALSE)
@@ -15,6 +16,12 @@ missions <- read.csv("demographics-religion/data/paulist-chronicles/paulist-miss
 canada <- map_data("world", "Canada")
 mexico <- map_data("world", "Mexico")
 us     <- map_data("state")
+rr_1861 <- readOGR(dsn = "/home/lmullen/research-data/USrailshps/RR1861/",
+                   layer = "RR1861WGS84")
+rr_1861 <- fortify(rr_1861)
+rr_1870 <- readOGR(dsn = "/home/lmullen/research-data/USrailshps/RR1870/",
+                   layer = "RR1870WGS84")
+rr_1870 <- fortify(rr_1870)
 
 # Three datasets
 missions_1 <- subset(missions, year < 1866)
@@ -47,17 +54,19 @@ map_1 <- base_map +
   geom_point(data = missions_1,
              aes(x = geo.lon, y=geo.lat, size = converts_total),
              alpha = 0.5) +
-  ggtitle("Redemptorist and Paulist Missions, 1852-1865") +
+  geom_path(data = rr_1861, aes(x = long, y= lat, group = group),
+            color = 'black', alpha = .5, size = .3) +
+  ggtitle("Redemptorist and Paulist Missions, 1852-1865, with Railroads in 1861")
 
-# pdf(file = "paulists-map-pre-civil-war.pdf",
-#     height = 8.5, width = 11)
+pdf(file = "paulists-map-1.pdf",
+    height = 8.5, width = 11)
 print(map_1)
-# dev.off()
+dev.off()
 
 # Map 2
 map_2 <- base_map +
-#   geom_path(data = rail, aes(x = long, y= lat, group = group),
-#             color = 'black', alpha = .5, size = .3) +
+  geom_path(data = rr_1870, aes(x = long, y= lat, group = group),
+            color = 'black', alpha = .5, size = .3) +
   xlim(-126,-65) +
   ylim(23, 51) +
   geom_point(data = missions_2,
@@ -65,23 +74,27 @@ map_2 <- base_map +
              alpha = 0.5) +
   ggtitle("Paulist Missions 1871-1882, with Railroads in 1870")
 
-# pdf(file = "outputs/paulists/paulists-map-post-civil-war.pdf",
-#     height = 8.5, width= 11)
+pdf(file = "paulists-map-2.pdf",
+    height = 8.5, width= 11)
 print(map_2)
-# dev.off()
+dev.off()
+system("pdftk paulists-map-2.pdf cat 1west
+       output paulists-map-2.pdf")
 
 # Map 3
 map_3 <- base_map +
-#   geom_path(data = rail, aes(x = long, y= lat, group = group),
-#             color = 'black', alpha = .5, size = .3) +
   xlim(-126,-65) +
   ylim(23, 51) +
   geom_point(data = missions_3,
              aes(x = geo.lon, y=geo.lat, size = converts_total),
              alpha = 0.5) +
+  geom_path(data = rr_1870, aes(x = long, y= lat, group = group),
+            color = 'black', alpha = .5, size = .3) +
   ggtitle("Paulist Missions 1883-1893, with Railroads in 1870")
 
-# pdf(file = "outputs/paulists/paulists-map-post-civil-war.pdf",
-#     height = 8.5, width= 11)
+pdf(file = "paulists-map-3.pdf",
+    height = 8.5, width= 11)
 print(map_3)
-# dev.off()
+dev.off()
+system("pdftk paulists-map-3.pdf cat 1west output paulists-map-3.pdf")
+
