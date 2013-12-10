5a850687264b4c430704dfd87225e90010e2e18f
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-12-10 18:49:25 -0500

    Append SVGs for the charts

--- mapping.js
+++ mapping.js
@@ -1,17 +1,34 @@
-var width = $("#map").width();
-var height = 500;
-console.log(width);
-console.log(height);
+var map_width = $("#map").width();
+var map_height = 500;
+var chart_width = $("#chart-1").width();
+var chart_height = 200;
 
 var projection = d3.geo.albersUsa()
 .scale(1000)
-.translate([width / 2, height / 2]);
+.translate([map_width / 2, map_height / 2]);
 
 var path = d3.geo.path().projection(projection);
 
-var svg = d3.select("#map").append("svg")
-.attr("width", width)
-.attr("height", height);
+// Create the SVGs
+var map_svg = d3.select("#map")
+.append("svg")
+.attr("width", map_width)
+.attr("height", map_height);
+
+var chart_1_svg = d3.select("#chart-1")
+.append("svg")
+.attr("width", chart_width)
+.attr("height", chart_height);
+
+var chart_2_svg = d3.select("#chart-2")
+.append("svg")
+.attr("width", chart_width)
+.attr("height", chart_height);
+
+var chart_3_svg = d3.select("#chart-3")
+.append("svg")
+.attr("width", chart_width)
+.attr("height", chart_height);
 
 queue()
 .defer(d3.json, "state_1870.json")
@@ -19,18 +36,18 @@ queue()
 .await(ready);
 
 function ready(error, state_1870, missions) {
-  svg.selectAll(".states")
+  map_svg.selectAll(".states")
   .data(topojson.feature(state_1870, state_1870.objects.states).features)
   .enter().append("path")
   .attr("class", function(d) { return "state " + d.id; })
   .attr("d", path);
 
-  svg.append("path")
+  map_svg.append("path")
   .datum(topojson.mesh(state_1870, state_1870.objects.states))
   .attr("d", path)
   .attr("class", "border");
 
-  svg.selectAll("circles.points")
+  map_svg.selectAll("circles.points")
   .data(missions)
   .enter()
   .append("circle")
@@ -48,7 +65,7 @@ function ready(error, state_1870, missions) {
       $("#current-year").text(ui.value);
       $("#all-checkbox").attr("checked", false);
 
-      svg.selectAll(".mission")
+      map_svg.selectAll(".mission")
       .data(missions)
       .classed("hidden", true)
       .filter(function(d) {
@@ -62,13 +79,13 @@ function ready(error, state_1870, missions) {
   $("#all-checkbox").click( function() {
     if ($("#all-checkbox").prop('checked')) {
       $("#current-year").text("1851-1907");
-      svg.selectAll(".mission")
+      map_svg.selectAll(".mission")
       .data(missions)
       .classed("hidden", false);
     } else {
       var current_year = $('#year-selector').slider("option", "value");
       $("#current-year").text(current_year);
-      svg.selectAll(".mission")
+      map_svg.selectAll(".mission")
       .data(missions)
       .classed("hidden", true)
       .filter(function(d) {
