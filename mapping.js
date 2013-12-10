5d811618f72119e2a358319b24010fa4a33fbaa3
Lincoln Mullen <lincoln@lincolnmullen.com>
2013-12-10 15:02:30 -0500

    Use queue.js instead of nested functions

--- mapping.js
+++ mapping.js
@@ -11,9 +11,12 @@ var svg = d3.select("body").append("svg")
 .attr("width", width)
 .attr("height", height);
 
+queue()
+.defer(d3.json, "state_1870.json")
+.defer(d3.csv, "missions.csv")
+.await(ready);
 
-d3.json("state_1870.json", function(error, state_1870) {
-
+function ready(error, state_1870, missions) {
   svg.selectAll(".states")
   .data(topojson.feature(state_1870, state_1870.objects.states).features)
   .enter().append("path")
@@ -25,62 +28,53 @@ d3.json("state_1870.json", function(error, state_1870) {
   .attr("d", path)
   .attr("class", "border");
 
-  d3.csv("missions.csv", function(error, missions) {
-    svg.selectAll("circles.points")
-    .data(missions)
-    .enter()
-    .append("circle")
-    // .attr("r", function(d) {return 2 * Math.sqrt(d.converts)})
-    .attr("r", 3)
-    .attr("class","mission")
-    .attr("transform", function(d) {return "translate(" + projection([d.lon,d.lat]) + ")";});
+  svg.selectAll("circles.points")
+  .data(missions)
+  .enter()
+  .append("circle")
+  // .attr("r", function(d) {return 2 * Math.sqrt(d.converts)})
+  .attr("r", 3)
+  .attr("class","mission")
+  .attr("transform", function(d) {return "translate(" + projection([d.lon,d.lat]) + ")";});
 
+  // Setup the slider to select the year
+  $("#year-selector").slider({
+    min: 1851,
+    max: 1907,
+    step: 1,
+    slide: function ( event, ui ) {
+      $("#current-year").text(ui.value);
+      $("#all-checkbox").attr("checked", false);
 
-    // Setup the slider to select the year
-    $("#year-selector").slider({
-      min: 1851,
-      max: 1907,
-      step: 1,
-      slide: function ( event, ui ) {
-        $("#current-year").text(ui.value);
-        $("#all-checkbox").attr("checked", false);
+      svg.selectAll(".mission")
+      .data(missions)
+      .classed("hidden", true)
+      .filter(function(d) {
+        return +d.year === ui.value;
+      })
+      .classed("hidden", false);
 
-        svg.selectAll(".mission")
-        .data(missions)
-        .classed("hidden", true)
-        .filter(function(d) {
-          return +d.year === ui.value;
-        })
-        .classed("hidden", false);
-
-      }
-    });
-
-    $("#all-checkbox").click( function() {
-      if ($("#all-checkbox").prop('checked')) {
-        $("#current-year").text("1851-1907");
-        svg.selectAll(".mission")
-        .data(missions)
-        .classed("hidden", false);
-      } else {
-        var current_year = $('#year-selector').slider("option", "value");
-        $("#current-year").text(current_year);
-        svg.selectAll(".mission")
-        .data(missions)
-        .classed("hidden", true)
-        .filter(function(d) {
-          return +d.year === current_year;
-        })
-        .classed("hidden", false);
-      }
-    });
-
-    // Set the initial value of the current year
-    // $( "#current-year" ).text($( "#year-selector" ).slider("value"));
-
-
-
+    }
   });
 
-});
+  $("#all-checkbox").click( function() {
+    if ($("#all-checkbox").prop('checked')) {
+      $("#current-year").text("1851-1907");
+      svg.selectAll(".mission")
+      .data(missions)
+      .classed("hidden", false);
+    } else {
+      var current_year = $('#year-selector').slider("option", "value");
+      $("#current-year").text(current_year);
+      svg.selectAll(".mission")
+      .data(missions)
+      .classed("hidden", true)
+      .filter(function(d) {
+        return +d.year === current_year;
+      })
+      .classed("hidden", false);
+    }
+  });
+
+}
 
