8a79b90638643fd0164d31f8d47de0738a6d264d
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-01-06 18:07:43 -0500

    Added axes to charts

--- mapping.js
+++ mapping.js
@@ -1,7 +1,8 @@
 var map_width = $("#map").width();
 var map_height = 500;
-var chart_width = $("#chart-1").width();
-var chart_height = 200;
+var chart_margin = {top: 10, right: 50, bottom: 10, left: 0};
+var chart_width = $("#chart-1").width() - chart_margin.left - chart_margin.right;
+var chart_height = 200 - chart_margin.top - chart_margin.bottom;
 
 var projection = d3.geo.albersUsa()
 .scale(1000)
@@ -17,18 +18,18 @@ var map_svg = d3.select("#map")
 
 var chart_1 = d3.select("#chart-1")
 .append("svg")
-.attr("width", chart_width)
-.attr("height", chart_height);
+.attr("width", chart_width + chart_margin.left + chart_margin.right)
+.attr("height", chart_height + chart_margin.top + chart_margin.bottom);
 
 var chart_2 = d3.select("#chart-2")
 .append("svg")
-.attr("width", chart_width)
-.attr("height", chart_height);
+.attr("width", chart_width + chart_margin.left + chart_margin.right)
+.attr("height", chart_height + chart_margin.top + chart_margin.bottom);
 
 var chart_3 = d3.select("#chart-3")
 .append("svg")
-.attr("width", chart_width)
-.attr("height", chart_height);
+.attr("width", chart_width + chart_margin.left + chart_margin.right)
+.attr("height", chart_height + chart_margin.top + chart_margin.bottom);
 
 queue()
 .defer(d3.json, "state_1870.json")
@@ -60,15 +61,30 @@ function ready(error, state_1870, missions, summary) {
   // Scales for charts
   var scale_1 = d3.scale.linear()
   .domain([0, d3.max(summary, function(d) {return +d.missions_total})])
-  .range([0, chart_height]);
+  .range([chart_height, 0]);
+
+  var axis_1 = d3.svg.axis()
+  .scale(scale_1)
+  .orient("right")
+  .ticks(5);
 
   var scale_2 = d3.scale.linear()
   .domain([0, d3.max(summary, function(d) {return +d.confessions})])
-  .range([0, chart_height]);
+  .range([chart_height, 0]);
+
+  var axis_2 = d3.svg.axis()
+  .scale(scale_2)
+  .orient("right")
+  .ticks(5);
 
   var scale_3 = d3.scale.linear()
   .domain([0, d3.max(summary, function(d) {return +d.converts})])
-  .range([0, chart_height]);
+  .range([chart_height, 0]);
+
+  var axis_3 = d3.svg.axis()
+  .scale(scale_3)
+  .orient("right")
+  .ticks(5);
 
   var bar_width = chart_width / summary.length;
 
@@ -78,27 +94,42 @@ function ready(error, state_1870, missions, summary) {
   .enter()
   .append("rect")
   .attr("x", function(d, i) { return i * bar_width; })
-  .attr("y", function(d, i) { return chart_height - scale_1(+d.missions_total); })
+  .attr("y", function(d, i) { return scale_1(+d.missions_total); })
   .attr("width", bar_width)
-  .attr("height", function(d, i) { return scale_1(+d.missions_total); })
+  .attr("height", function(d, i) { return chart_height - scale_1(+d.missions_total); })
+
+  chart_1.append("g")
+  .attr("class", "y axis")
+  .call(axis_1)
+  .attr("transform", "translate(" + (chart_width + 1) + ",0)")
 
   chart_2.selectAll("rect")
   .data(summary)
   .enter()
   .append("rect")
   .attr("x", function(d, i) { return i * bar_width; })
-  .attr("y", function(d, i) { return chart_height - scale_2(+d.confessions); })
+  .attr("y", function(d, i) { return scale_2(+d.confessions); })
   .attr("width", bar_width)
-  .attr("height", function(d, i) { return scale_2(+d.confessions); })
+  .attr("height", function(d, i) { return chart_height - scale_2(+d.confessions); })
+
+  chart_2.append("g")
+  .attr("class", "y axis")
+  .call(axis_2)
+  .attr("transform", "translate(" + (chart_width + 1) + ",0)")
 
   chart_3.selectAll("rect")
   .data(summary)
   .enter()
   .append("rect")
   .attr("x", function(d, i) { return i * bar_width; })
-  .attr("y", function(d, i) { return chart_height - scale_3(+d.converts); })
+  .attr("y", function(d, i) { return scale_3(+d.converts); })
   .attr("width", bar_width)
-  .attr("height", function(d, i) { return scale_3(+d.converts); })
+  .attr("height", function(d, i) { return chart_height - scale_3(+d.converts); })
+
+  chart_3.append("g")
+  .attr("class", "y axis")
+  .call(axis_3)
+  .attr("transform", "translate(" + (chart_width + 1) + ",0)")
 
   // Setup the slider to select the year
   $("#year-selector").slider({
